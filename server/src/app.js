63fa0cc85bb2d09d9cd3b1c153307ec4d3e4b0ff
anjpai <rpaianju15@gmail.com>
2025-06-01 20:16:02 +0530

    Final

--- server/src/app.js
+++ server/src/app.js
@@ -4,12 +4,24 @@ const helmet = require('helmet');
 const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
 
 const authRouter = require('./routes/api/auth');
 const companiesRouter = require('./routes/api/companies');
 const studentsRouter = require('./routes/api/students');
 const jobsRouter = require('./routes/api/jobs');
 const profileRouter = require('./routes/api/profile');
+
+
+// Collect default metrics every 10 seconds
+collectDefaultMetrics({ timeout: 10000 });
+// Optional: HTTP request counter
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'statusCode']
+});
 
 mongoose
   .connect(process.env.MONGO_DB_URI)
@@ -24,12 +36,30 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Middleware to count HTTP requests
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    httpRequestCounter.inc({
+      method: req.method,
+      route: req.url,
+      statusCode: res.statusCode
+    });
+  });
+  next();
+});
 app.use('/api/user', authRouter);
 app.use('/api/companies', companiesRouter);
 app.use('/api/students', studentsRouter);
 app.use('/api/jobs', jobsRouter);
 app.use('/api/profile', profileRouter);
-
+app.get('/metrics', async (req, res) => {
+  try {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+  } catch (err) {
+    res.status(500).end(err);
+  }
+});
 app.use((req, res) => res.status(404).send('404 - Not Found'));
 
 module.exports = app;
